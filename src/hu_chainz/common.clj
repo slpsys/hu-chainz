01ce43c00529c3b720c9f3ead2eb60dc92e44749
Marc Bollinger <marc@lumoslabs.com>
2014-08-09 18:37:44 -0700

    Simplifying/updating ingest chain

--- src/hu_chainz/common.clj
+++ src/hu_chainz/common.clj
@@ -1,14 +1,18 @@
 (ns hu-chainz.common)
 
+; Int funcs
 (def incr (partial + 1))
 
 (def decr (partial - 1))
 
-(def zip (partial map vector))
+; Seq funcs
+(def first-string (comp str first))
 
 (defn has-more? [my-seq] (if (seq (rest my-seq)) true false))
 
+(def zip (partial map vector))
+
+; Logic funcs
 ; Is this defined somewhere common? It has to be, right?
 (defmacro unless [pred a b]
 	`(if (not ~pred) ~a ~b))
-
--- src/hu_chainz/core.clj
+++ src/hu_chainz/core.clj
@@ -6,16 +6,37 @@
   [line]
   (clojure.string/split line #"[\s\t\n]"))
 
-(def first-string (comp str first))
-
 (defn token-to-chain
-  ""
+  "Converts a sequence of tokens to a sequence of ordered pairs,
+    e.g. ('a' 'b') => ([:start 'a'] ['a' 'b'] ['b' :end])"
   [token-list]
   (let [first-pair [:start (first token-list)]]
     (cond
       (not (seq token-list)) [[:start :end]]
       (not (has-more? token-list)) [first-pair [(first token-list) :end]]
       :else (cons first-pair (zip token-list (conj (vec (rest token-list)) :end))))))
+
+(defn update-map
+  [token-pair directed-map]
+  (let [first-token (first token-pair)
+        second-token (second token-pair)]
+    (unless (contains? directed-map first-token)
+      (assoc directed-map first-token {second-token 1 })
+      (let [inner-map (get directed-map first-token)]
+        (assoc directed-map
+               first-token
+               (if (get inner-map second-token)
+                 (assoc inner-map
+                             second-token
+                             (incr (get inner-map second-token)))
+                 (assoc inner-map second-token 1)))))))
+
+(defn token-to-map
+  [token-chain existing-map]
+  (let [updated-map (update-map (first token-chain) existing-map)]
+    (if (has-more? token-chain)
+      (recur (rest token-chain) updated-map)
+      updated-map)))
 
 (defn construct-state-map
   ""
