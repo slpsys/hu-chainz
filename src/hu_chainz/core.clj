ee932bc76166268cc8c7c8ad19565bec13450cd2
Marc Bollinger <marc@lumoslabs.com>
2014-08-10 15:47:44 -0700

    Model building now end-to-end complete.

--- src/hu_chainz/core.clj
+++ src/hu_chainz/core.clj
@@ -41,5 +41,19 @@
 
 (defn textulate
   "I don't do a whole lot."
-  [text]
-  (-> text tokenize token-list-to-chain chain-to-map))
+  [text & {:keys [tokenizer-fn] :or {tokenizer-fn tokenize}}]
+    (-> text tokenizer-fn token-list-to-chain chain-to-map))
+
+(defn merge-nested-maps
+  [larger-map smaller-map]
+  (let [unconflicted-keys (clojure.set/difference (set (keys smaller-map)) (set (keys larger-map)))
+        conflicted-keys (clojure.set/intersection (set (keys smaller-map)) (set (keys larger-map)))
+        even-larger-map (merge larger-map (select-keys smaller-map unconflicted-keys))]
+    (merge even-larger-map
+           (reduce #(assoc %1 %2 (merge-with + (get larger-map %2) (get smaller-map %2))) {} conflicted-keys))))
+
+(defn feed
+  [corpus & {:keys [tokenizer-fn phrase-boundary-fn]
+              :or {tokenizer-fn tokenize phrase-boundary-fn clojure.string/split-lines}}]
+    (let [phrases (phrase-boundary-fn corpus)]
+      (reduce merge-nested-maps (map textulate phrases))))
