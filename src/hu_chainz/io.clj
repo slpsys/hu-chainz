ef6a63f7731e8ed0275cedfc4c0c37beb9fdea10
Marc Bollinger <marc@lumoslabs.com>
2015-10-02 14:54:33 -0700

    Helper functions to tell you about something you're interested in.

--- src/hu_chainz/io.clj
+++ src/hu_chainz/io.clj
@@ -7,16 +7,21 @@
 
 (defn stringify [tokens] (clojure.string/join " " tokens))
 
+(def talk (comp stringify generate))
+
+(defn tell-me-about
+  "Builds strings randomly until it finds one that contains a particular string. Useful for proper nouns you know are in the corpus."
+  ([something] (tell-me-about something 1000000))
+  ([something up-to]
+   (take 1
+     (filter
+       #(.contains % something)
+       (repeatedly up-to #(talk my-model))))))
+
 (defn create-model
   "Builds a model from a local filename."
   [fname]
   (-> fname slurp feed))
-
-(defn write-model
-  "Writes a model to disk."
-  [model fname]
-  (with-open [fh (clojure.java.io/output-stream fname)]
-             (t/write (t/writer fh :msgpack) model)))
 
 (defn read-model
   [fname]
@@ -24,3 +29,9 @@
     [in-stream (io/input-stream fname)
      rdr       (t/reader in-stream :msgpack)]
   (t/read rdr)))
+
+(defn write-model
+  "Writes a model to disk."
+  [model fname]
+  (with-open [fh (clojure.java.io/output-stream fname)]
+             (t/write (t/writer fh :msgpack) model)))
